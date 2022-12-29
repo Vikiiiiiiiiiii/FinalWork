59d22b83da8b973b150143eb2363a244695afb11
Viktoriya <Vik@gmail.com>
2022-12-29 12:03:22 +0300

    заполняем массив

---
+++ Задача/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше либо равна 3 символа.*/
+
+
+int[] InitArray(int length) 
+{     
+    int[] array = new int[length];     
+    Random rnd = new Random();    
+    for (int i = 0; i < length; i++)    
+    {         
+        array[i] = rnd.Next(10,50);     
+    }      
+    return array; 
+}  
+
